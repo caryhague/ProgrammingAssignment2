6b9bb74af44ea2d357f351410c7b3ee71c1ec3b8
Cary Hague <caryhague@yahoo.com>
2014-05-22 20:09:01 -0500

    work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## cacheSolve uses the cache if available to solve the inverse of the matrix
 
-## Write a short comment describing this function
+## function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to:
+    ## set the value of the vector
+    ## get the value of the vector
+    ## set the value of the mean
+    ## get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+## should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    time1 <- proc.time() 
+    m <- x$getinv()
+    if(!is.null(m)) {
+        ## get the cached data
+        cat("Time:", proc.time()-time1)
+        return(m)
+    }
+    mydata1 <- x$get()
+    m <- solve(mydata1, ...)
+    x$setinv(m)
+    cat("Time:", proc.time() - time1)
+    return(m)
 }
